245be5d196256f548bf9ce23b4672d94da7f9ce9
shbshb99 <shbshb1212@gmail.com>
2025-11-06 14:57:39 +0900

    config

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
-export const WEDDING_INVITATION_URL = "https://hb-ys.netlify.app/";
+export const WEDDING_INVITATION_URL = "https://g1-pinetre.netlify.app/";
 export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
-export const KAKAOTALK_SHARE_IMAGE =
-  "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
+export const KAKAOTALK_SHARE_IMAGE = "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "2025.8.31";
-export const WEDDING_LOCATION = "SUN 13:40";
+export const WEDDING_DATE = "2026.2.14";
+export const WEDDING_LOCATION = "SAT 13:40";
 
-export const GROOM_NAME = "신현범";
-export const GROOM_ACCOUNT_NUMBER = "새마을금고 9003-2177-3837-3";
-export const GROOM_FATHER_NAME = "신혜성";
-export const GROOM_FATHER_ACCOUNT_NUMBER = "신협 132-094-528952";
-export const GROOM_MOTHER_NAME = "정윤경";
+export const GROOM_NAME = "박푸른솔";
+export const GROOM_ACCOUNT_NUMBER = "케이뱅크 100022053077";
+export const GROOM_FATHER_NAME = "박재동";
+export const GROOM_FATHER_ACCOUNT_NUMBER = "신한110501919049";
+export const GROOM_MOTHER_NAME = "김은혜";
 
-export const BRIDE_NAME = "김예손";
-export const BRIDE_ACCOUNT_NUMBER = "카카오뱅크 3333-24-8556798";
-export const BRIDE_FATHER_NAME = "김영호";
-export const BRIDE_MOTHER_NAME = "오교선";
-export const BRIDE_MOTHER_ACCOUNT_NUMBER = "농협 352-1260-4758-43";
+export const BRIDE_NAME = "곽지원";
+export const BRIDE_ACCOUNT_NUMBER = "기업은행01037853157";
+export const BRIDE_FATHER_NAME = "곽병권";
+export const BRIDE_MOTHER_NAME = "이경하";
+export const BRIDE_MOTHER_ACCOUNT_NUMBER = "하나 77891026815207";
+
+
